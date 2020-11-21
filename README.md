d55dbc4ed710cae7d5591bb30ed91bfaeca21940
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2020-11-20 21:38:55 -0700

    added practice gif and includes a generated readme

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 
-    # ReadMe-Generator
+    # README-Generator
 
     ## Motivation
-    This app is my first chance to work on the back end. I wanted to use my knowledge of NODE.js as well as built-in methods to write a file.
+    This was my first dive into the back end. It was an opportunity to utilize NODE.js and the Inquirer packages to run command line prompts and write to a file. I wanted the README to be clean and functional for the user.
+
+    ## How to Use
+    To use, clone locally and run 'node index.js' in the terminal. Answer the questions as you would like them to appear in the README.
+
+    ## Installation
+    Clone or fork this repo
+
+    ## Mock-Up
+    ![readme-generator-demo](./assets/readme-generator.gif)
+
+    ## Deployment
+    Link to [GitHub Repository](https://github.com/rayna-v/README-generator)
+    link to [Live URL]( https://rayna-v.github.io/README-generator/)
+
+    ## How to Report Issues
+    Please email me at raveuleman@gmail.com
+
+    ## How to Contribute
+    I would love to hear your suggestions via email.
     
--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const questions = ['What is the name of your project or repo?',
     'What is your motivation behind this app?',
     'How do you use this app?',
     'How is this app installed?',
-    'How can a user report issues?',
-    'How can a user make contributions?',
     'Enter link or path to image or gif of app.',
     'Enter alt text for that image or gif.',
     'What is your GitHub Repo URL?',
-    'What is the live URL?'
+    'What is the live URL?',
+    'How can a user report issues?',
+    'How can a user make contributions?',
 ];
 const fs = require('fs');
 const inquirer = require('inquirer');
@@ -20,6 +20,25 @@ function writeToFile(fileName, data) {
 
     ## Motivation
     ${data.motivation}
+
+    ## How to Use
+    ${data.howToUse}
+
+    ## Installation
+    ${data.installation}
+
+    ## Mock-Up
+    ![${data.imageAlt}](${data.imageLink})
+
+    ## Deployment
+    Link to [GitHub Repository](${data.gitHubURL})
+    link to [Live URL](${data.liveURL})
+
+    ## How to Report Issues
+    ${data.reportIssues}
+
+    ## How to Contribute
+    ${data.contribute}
     `
     fs.writeFile('README.md', userResponse, (err) =>
         err ? console.log(err) : console.log('complete')
@@ -38,12 +57,12 @@ function init() {
     const question2 = new Prompts('input', questions[1], 'motivation')
     const question3 = new Prompts('input', questions[2], 'howToUse')
     const question4 = new Prompts('input', questions[3], 'installation')
-    const question5 = new Prompts('input', questions[4], 'reportIssues')
-    const question6 = new Prompts('input', questions[5], 'contribute')
-    const question7 = new Prompts('input', questions[6], 'imageLink')
-    const question8 = new Prompts('input', questions[7], 'imageAlt')
-    const question9 = new Prompts('input', questions[8], 'gitHubURL')
-    const question10 = new Prompts('input', questions[9], 'liveURL')
+    const question5 = new Prompts('input', questions[4], 'imageLink')
+    const question6 = new Prompts('input', questions[5], 'imageAlt')
+    const question7 = new Prompts('input', questions[6], 'gitHubURL')
+    const question8 = new Prompts('input', questions[7], 'liveURL')
+    const question9 = new Prompts('input', questions[8], 'reportIssues')
+    const question10 = new Prompts('input', questions[9], 'contribute')
 
 
     inquirer
@@ -59,46 +78,46 @@ function init() {
                 message: question2.message,
                 name: question2.name,
             },
-            // {
-            //     type: question3.type,
-            //     message: question3.message,
-            //     name: question3.name,
-            // },
-            // {
-            //     type: question4.type,
-            //     message: question4.message,
-            //     name: question4.name,
-            // },
-            // {
-            //     type: question5.type,
-            //     message: question5.message,
-            //     name: question5.name,
-            // },
-            // {
-            //     type: question6.type,
-            //     message: question6.message,
-            //     name: question6.name,
-            // },
-            // {
-            //     type: question7.type,
-            //     message: question7.message,
-            //     name: question7.name,
-            // },
-            // {
-            //     type: question8.type,
-            //     message: question8.message,
-            //     name: question8.name,
-            // },
-            // {
-            //     type: question9.type,
-            //     message: question9.message,
-            //     name: question9.name,
-            // },
-            // {
-            //     type: question10.type,
-            //     message: question10.message,
-            //     name: question10.name,
-            // },
+            {
+                type: question3.type,
+                message: question3.message,
+                name: question3.name,
+            },
+            {
+                type: question4.type,
+                message: question4.message,
+                name: question4.name,
+            },
+            {
+                type: question5.type,
+                message: question5.message,
+                name: question5.name,
+            },
+            {
+                type: question6.type,
+                message: question6.message,
+                name: question6.name,
+            },
+            {
+                type: question7.type,
+                message: question7.message,
+                name: question7.name,
+            },
+            {
+                type: question8.type,
+                message: question8.message,
+                name: question8.name,
+            },
+            {
+                type: question9.type,
+                message: question9.message,
+                name: question9.name,
+            },
+            {
+                type: question10.type,
+                message: question10.message,
+                name: question10.name,
+            },
 
         ])
 
